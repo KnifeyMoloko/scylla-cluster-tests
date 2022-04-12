3eb35a95dee13904101be96e9092da0c978d30cb
MaciekCisowski <maciej.cisowski@scylladb.com>
2022-04-12 21:16:19 +0200

    fix(rolling_upgrade): improve error handling and logging

--- vars/rollingUpgradePipeline.groovy
+++ vars/rollingUpgradePipeline.groovy
@@ -99,17 +99,22 @@ def call(Map pipelineParams) {
                                 wrap([$class: 'BuildUser']) {
                                     dir('scylla-cluster-tests') {
                                         checkout scm
-                                        ArrayList base_versions_list = params.base_versions.contains('.') ? params.base_versions.split('\\,') : []
-                                        supportedVersions = supportedUpgradeFromVersions(
-                                            base_versions_list,
-                                            pipelineParams.linux_distro,
-                                            params.new_scylla_repo
-                                        )
-                                        (testDuration,
-                                         testRunTimeout,
-                                         runnerTimeout,
-                                         collectLogsTimeout,
-                                         resourceCleanupTimeout) = getJobTimeouts(params, builder.region)
+                                        try {
+                                            ArrayList base_versions_list = params.base_versions.contains('.') ? params.base_versions.split('\\,') : []
+                                            supportedVersions = supportedUpgradeFromVersions(
+                                                base_versions_list,
+                                                pipelineParams.linux_distro,
+                                                params.new_scylla_repo
+                                            )
+                                        } catch (Exception e) {
+                                            println(e)
+                                            error("Run aborted due to inability to list supported upgrade-from versions.")
+                                        }
+                                            (testDuration,
+                                             testRunTimeout,
+                                             runnerTimeout,
+                                             collectLogsTimeout,
+                                             resourceCleanupTimeout) = getJobTimeouts(params, builder.region)
                                     }
                                 }
                             }
